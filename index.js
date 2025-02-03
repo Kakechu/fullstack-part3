b2b6a32c66515743196773f806a51ad76bedce13
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-02-03 22:58:58 +0200

    Task 3.2

--- index.js
+++ index.js
@@ -24,12 +24,22 @@ let persons = [
     }
 ]
 
+
+let date = new Date()
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${String(date)}</p>
+    `)
 })
 
 const PORT = 3001
