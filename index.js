7b1b48fe71877eb017bb976a18a9a2d5673ba3aa
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-02-04 10:35:14 +0200

    Task 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       "id": "1",
@@ -58,6 +60,19 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const id = Math.floor(Math.random() * 100000)
+
+    const person = request.body
+    person.id = String(id)
+    console.log(person.id)
+
+    persons = persons.concat(person)
+
+    console.log(person)
+    response.json(person)
 })
 
 
