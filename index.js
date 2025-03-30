565a72b110c7ff3a6008f798ce89e028de2f50d2
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-03-30 14:50:43 +0300

    Tasks 3.12-3.15

--- index.js
+++ index.js
@@ -16,42 +16,7 @@ morgan.token('body', (request) => {
 
 
 
-
-
-//app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-
-
-
-/*
-let persons = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"      
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"      
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"      
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"      
-    }
-]
-*/
-
-
-const cors = require('cors')
-app.use(cors())
 
 app.use(express.static('dist'))
 
@@ -79,29 +44,23 @@ app.get('/api/persons/:id', (request, response) => {
         response.json(person)
     })
     
-    /*
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-    */
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => {
+        console.log(error)
+        response.status(400).send( {error: 'malformatted id'} )
+      })
 
-    response.status(204).end()
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
     console.log("body", body)
-    //const person = request.body
 
     if (!body.name) {
         return response.status(400).json({
@@ -132,12 +91,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
-    //const id = Math.floor(Math.random() * 100000)
-    //person.id = String(id)
-    
-    //persons = persons.concat(person)
 
-    //response.json(person)
 })
 
 
